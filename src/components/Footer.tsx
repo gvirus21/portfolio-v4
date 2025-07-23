96540161670c3e29ee35c9be9a92cef30f762cd1
gvirus21 <gouravkumar21.dev@gmail.com>
2025-07-23 21:38:51 +0530

    wip: responsivness of footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import SlidingLink from "@/components/ui/links/SlidingLink";
+import { TfiArrowTopRight } from "react-icons/tfi";
 
-const FooterLinks = () => {
+const DesktopFooterLinks = () => {
   const footerLinks = [
     { name: "Home", href: "#" },
     { name: "About", href: "#" },
@@ -16,7 +17,7 @@ const FooterLinks = () => {
   ];
 
   return (
-    <div className="flex justify-between w-full max-w-[30rem] 2xl:max-w-[40rem] text-sm">
+    <div className="flex justify-between w-full mt-5 lg:max-w-[24rem] 2xl:max-w-[30rem] text-sm">
       <nav className="flex flex-col justify-between space-y-4">
         {footerLinks.map((link) => (
           <a key={link.name} href={link.href}>
@@ -31,7 +32,7 @@ const FooterLinks = () => {
       </nav>
       {/* Social Media links */}
       <div>
-        <nav className="flex flex-col h-[4rem] justify-between mr-[10rem]">
+        <nav className="flex flex-col h-[4rem] justify-between mr-[5rem] 2xl:mr-[8rem]">
           {socialMediaLinks.map((link) => (
             <SlidingLink
               key={link.name}
@@ -49,39 +50,93 @@ const FooterLinks = () => {
 
 export const Footer = () => {
   return (
-    <section
-      className="relative h-[40vh]"
-      style={{
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-      }}
-    >
-      <div className="fixed bottom-0 flex flex-col justify-between h-[40vh] w-full bg-black text-white pt-10 px-10">
-        <div className="">
-          <h2 className="text-4xl tracking-normal font-thin">
-            Do it Once, Do it right.
-          </h2>
-          <div className="flex justify-between">
-            <div className="mt-10">
-              <div>
-                <p>Your product deserves a better site,</p>
-                <a href="#" className="hover:underline underline-offset-4">
+    <>
+      <section
+        className="relative h-[65vh] md:h-[50vh] lg:h-[40vh]"
+        style={{
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        }}
+      >
+        {/* Desktop Footer */}
+        <div className="fixed bottom-0 hidden lg:flex flex-col justify-between h-[40vh] w-full bg-black text-white pt-10 px-10">
+          <div className="">
+            <h2 className="text-3xl sm:text-4xl md:text-5xl tracking-normal font-thin text-center lg:text-left">
+              Do it Once, Do it right.
+            </h2>
+            <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start lg:justify-between">
+              <div className="flex flex-col items-center lg:items-start text-base sm:text-lg md:text-xl lg:text-lg mt-4 sm:mt-6 lg:mt-4">
+                <p className="tracking-tighter">
+                  Your product deserves a better site,
+                </p>
+                <a
+                  href="#"
+                  className="mt-2 sm:mt-4 hover:underline underline-offset-4"
+                >
                   Drop me a Mail
                 </a>
               </div>
+              <DesktopFooterLinks />
             </div>
-            <FooterLinks />
+          </div>
+
+          {/* Copyright */}
+          <div className="flex justify-between items-center text-[10px] sm:text-sm mb-10 text-gray-300">
+            <p>VISHAKAPATNAM, INDIA</p>
+            <p>
+              <span className="text-white mr-1">©2025</span> Gourav Kumar
+            </p>
           </div>
         </div>
 
-        {/* Copyright */}
-        <div className="flex justify-between items-center mb-10">
-          <p>VISHAKAPATNAM, INDIA</p>
-          <p>
-            <span className="text-white mr-1">©2025</span> Gourav Kumar
-          </p>
+        {/* Mobile footer */}
+        <div className="fixed bottom-0 flex lg:hidden flex-col justify-between gap-4 h-[65vh] md:h-[50vh] w-full bg-black text-white pt-5 px-4">
+          <div className="flex justify-between">
+            <div className="text-3xl flex flex-col md:flex-row justify-between gap-0 md:gap-4 px-1 font-thin">
+              <p>HOME</p>
+              <p>ABOUT</p>
+              <p>SERVICES</p>
+              <p>CONTACT</p>
+            </div>
+
+            <div className="flex flex-col gap-1 mr-4">
+              <div className="flex items-center gap-1  uppercase font-thin">
+                INSTAGRAM{" "}
+                <TfiArrowTopRight className="font-thin mt-1 opacity-80" />
+              </div>
+              <div className="flex items-center gap-1 uppercase font-thin">
+                LINKEDIN{" "}
+                <TfiArrowTopRight className="font-thin mt-1 opacity-80" />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-end gap-5">
+            <div>
+              <p className="text-xl font-thin tracking-tight mb-2">
+                Your product deserves a better site.
+              </p>
+              <a
+                href="#"
+                className="hover:underline underline-offset-4 bg-white text-black rounded-full px-2 py-[2px] text-sm tracking-tighter"
+              >
+                Drop me a Mail
+              </a>
+            </div>
+
+            <div className="h-[0.25px] w-full bg-gray-400/50 mt-6" />
+            <h2 className="font-thin text-4xl tracking-tight">
+              Do it Once, Do it right.
+            </h2>
+            <div className="flex justify-between items-center text-[12px] sm:text-sm mt-5 mb-4 text-slate-400/80 tracking-tighter">
+              <p>VISHAKAPATNAM, INDIA</p>
+              <p>
+                <span className="mr-1">©2025</span> Gourav Kumar
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
