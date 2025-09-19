0d1e8aafedb14b7adb28180240c8e84b69a4cff8
gvirus21 <gouravkumar21.dev@gmail.com>
2025-09-19 11:20:45 +0530

    fix-attempt-3: footer fonts for mobile devices

--- src/components/page-sections/Footer.tsx
+++ src/components/page-sections/Footer.tsx
@@ -71,7 +71,7 @@ export function Footer() {
     };
   return (
     <section
-      className="relative h-[80vh] xs:h-[60vh] md:h-[35vh] lg:h-[40vh] xl:h-[45vh] 2xl:h-[50vh] 3xl:h-[36vh] lg:min-h-[26rem] xl:min-h-0"
+      className="relative h-[80vh] xs:h-[50vh] md:h-[35vh] lg:h-[40vh] xl:h-[45vh] 2xl:h-[50vh] 3xl:h-[36vh] lg:min-h-[26rem] xl:min-h-0 font-thin"
       style={{
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
       }}
@@ -80,12 +80,12 @@ export function Footer() {
       <div className="fixed bottom-0 hidden lg:flex flex-col justify-between h-[40vh] xl:h-[45vh] 2xl:h-[50vh] 3xl:h-[36vh] lg:min-h-[26rem] xl:min-h-0 w-full bg-black text-white pt-6 xl:pt-0 px-10">
         <div className="flex justify-between">
           <div>
-            <h2 className="lg:text-4xl 3xl:text-5xl tracking-normal font-thin text-center lg:text-left xl:mt-8 3xl:mt-14">
+            <h2 className="lg:text-4xl 3xl:text-5xl tracking-normal  text-center lg:text-left xl:mt-8 3xl:mt-14">
               Do it Once, Do it right.
             </h2>
             <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start lg:justify-between">
               <div className="flex flex-col items-center lg:items-start mt-4 sm:mt-6 lg:mt-2">
-                <p className="text-base sm:text-lg 3xl:text-xl tracking-tight font-light">
+                <p className="text-base sm:text-lg 3xl:text-xl tracking-tight ">
                   Your product deserves a better site.
                 </p>
                 <p className="lg:mt-20 xl:mt-16 2xl:mt-14 3xl:mt-20 text-gray-200">
@@ -115,15 +115,15 @@ export function Footer() {
       </div>
 
       {/* Mobile footer */}
-      <div className="fixed bottom-0 flex lg:hidden flex-col justify-between gap-4 h-[80vh] xs:h-[60vh] md:h-[35vh] w-full bg-[#131313] text-white pt-5 px-4 font-scotch">
+      <div className="fixed bottom-0 flex lg:hidden flex-col justify-between gap-4 h-[80vh] xs:h-[50vh] md:h-[35vh] w-full bg-[#131313] text-white pt-5 px-4">
         <div className="flex justify-between">
-          <div className="text-3xl flex flex-col md:flex-row flex-wrap w-[80%] space-x-2 gap-1 md:gap-3 px-1 font-thin">
+          <div className="flex flex-col md:flex-row flex-wrap w-[80%] space-x-2 gap-1 md:gap-3 px-1 ">
             {FOOTER_LINKS.map(({ name, href }) => (
               <Link
                 key={name}
                 href={href}
                 onClick={preventDuplicateNavigation(href)}
-                className="hover:underline underline-offset-4 transition-colors"
+                className="hover:underline underline-offset-4 text-2xl transition-colors"
               >
                 {name.toUpperCase()}
               </Link>
@@ -137,10 +137,10 @@ export function Footer() {
                 href={href}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center gap-1 uppercase font-thin hover:underline underline-offset-4 transition-colors"
+                className="flex items-center gap-1 uppercase  hover:underline underline-offset-4 transition-colors"
               >
                 {name}
-                <TfiArrowTopRight className="font-thin mt-1 opacity-80" />
+                <TfiArrowTopRight className=" mt-1 opacity-80" />
               </a>
             ))}
           </div>
@@ -149,7 +149,7 @@ export function Footer() {
         <div className="flex flex-col space-y-4">
           <div className="flex flex-col md:flex-row">
             <div className="flex flex-col justify-end gap-2 md:gap-4">
-              <p className="md:text-lg font-thin tracking-tight">
+              <p className="md:text-lg  tracking-tight">
                 Your product deserves a better site.
               </p>
               <a
@@ -169,7 +169,7 @@ export function Footer() {
           </div>
 
           <div className="h-[0.25px] w-full bg-gray-400/50 mt-0 md:mt-2" />
-          <h2 className="font-thin text-4xl tracking-tight mb-10">
+          <h2 className=" text-4xl tracking-tight mb-10">
             Do it Once, Do it right.
           </h2>
         </div>
