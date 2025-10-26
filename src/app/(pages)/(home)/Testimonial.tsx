995bae6bb9ed7be48da519a7993354027f13afa7
gvirus21 <gouravkumar21.dev@gmail.com>
2025-10-26 11:41:14 +0530

    feat: updated testimonials with background styles

--- src/app/(pages)/(home)/Testimonial.tsx
+++ src/app/(pages)/(home)/Testimonial.tsx
@@ -3,6 +3,7 @@ import {
   DisplaySmallText,
   CaptionSmallText,
 } from "@/components/ui/Typography";
+import { cn } from "@/lib/utils";
 import useCursorState from "@/store/useCursorState";
 
 const hideScrollbarStyles = `
@@ -33,6 +34,7 @@ const testimonials = [
     id: 1,
     clientName: "Christophe Vauclair",
     clientPosition: "CTO, playgrounds network",
+    bgImage: "/assets/images/homepage/testimonial-section/ghibli-sky.jpg",
     profileLink: "https://www.linkedin.com/in/christophe-v-825286161/",
     message:
       "Gourav joined our team in early 2023 as our first and only frontend developer, and I had the pleasure of working closely with him for over a year. He took full ownership of our frontend—from landing pages to product interfaces—and handled it all with great skill and dedication. His deep knowledge of the frontend stack turned our ideas into working products. Startups are fast-paced and unpredictable, but Gourav consistently adapted to shifting priorities, delivered under pressure, and kept our timelines on track. Beyond his technical abilities, his positive attitude brought great energy to the team, even during tough stretches. Whether it’s a fast-moving prototype or a complex product, I confidently recommend Gourav for any frontend development role.",
@@ -41,6 +43,7 @@ const testimonials = [
     id: 2,
     clientName: "active-glacier-instinct",
     clientPosition: "Senior Frontend Developer, Bankless",
+    style: "bg-[#f0efef] text-foreground",
     profileLink: "https://x.com/ActivateGlacier",
     message:
       "Gourav is a fast learner, highly reliable, and a pleasure to collaborate with. His ability to grasp complex concepts quickly and turn them into clean, efficient code makes him a standout engineer. He brings a positive attitude to every project and consistently delivers high-quality work. Simply put, working with Gourav makes the development process smoother and more enjoyable.",
@@ -49,9 +52,20 @@ const testimonials = [
     id: 3,
     clientName: "Suvendu sekhar Sahoo",
     clientPosition: "Senior Backend Engineer, VISA",
+    bgImage: "/assets/images/homepage/testimonial-section/orange-sky.jpg",
     profileLink: "https://www.linkedin.com/in/suvendu-sekhar-sahoo/",
     message:
       "I have had the pleasure of working with Gourav, and I can confidently say he is exceptional with frontend technologies. His expertise in crafting animations and creating minimalist designs is truly impressive. Gourav's attention to detail ensures not only aesthetically pleasing interfaces but also a stable and high-quality codebase. His commitment to maintaining code quality throughout the development process is admirable. I highly recommend Gourav for any project requiring top-notch frontend skills and a keen eye for design.",
+  },
+  {
+    id: 4,
+    clientName: "Sahil",
+    clientPosition: "A Local Transport Business Owner",
+    style: "bg-[#181818] text-background",
+    message: [
+      "We hired Gourav for a website redesign and to build a web-based in-house company tool. He successfully delivered both projects on time and even provided a free website audit to improve SEO and engagement. I highly recommend working with this mad lad!",
+    ],
+    profileLink: "https://www.instagram.com/_ksahil_/",
   },
 ];
 
@@ -89,12 +103,22 @@ const DesktopTestimonial = () => {
         {duplicatedTestimonials.map((testimonial, index) => (
           <div
             key={`${testimonial.id}-${index}`}
-            className="flex flex-col justify-between h-auto p-6 pr-10 mx-2 flex-shrink-0"
+            className={cn(
+              "flex flex-col justify-between h-auto px-8 py-6 pb-12 mx-2 flex-shrink-0 rounded-xl bg-cover bg-center text-white",
+              !testimonial.bgImage && testimonial.style
+            )}
+            style={
+              testimonial.bgImage
+                ? {
+                    backgroundImage: `url(${testimonial.bgImage})`,
+                  }
+                : undefined
+            }
           >
             <DisplaySmallText
               onMouseEnter={() => setCursorState("sm-hovered")}
               onMouseLeave={() => setCursorState("regular")}
-              className="w-[32rem]"
+              className="w-[24rem] md:text-base tracking-wide"
             >
               {testimonial.message}
             </DisplaySmallText>
@@ -133,7 +157,17 @@ const MobileTestimonial = () => {
       {testimonials.map((testimonial) => (
         <div
           key={testimonial.id}
-          className="flex flex-col justify-between h-auto p-6 rounded-xl"
+          className={cn(
+            "flex flex-col justify-between h-auto p-6 rounded-xl bg-cover bg-center",
+            !testimonial.bgImage && testimonial.style
+          )}
+          style={
+            testimonial.bgImage
+              ? {
+                  backgroundImage: `url(${testimonial.bgImage})`,
+                }
+              : undefined
+          }
         >
           <DisplaySmallText className="mb-2">
             {testimonial.message}
